64f14199c4b2fd78e652d07e8315720b7566e9e8
Maximo Lopez <maximo.lopez@spatialnetworks.com>
2022-08-20 01:59:35 -0400

    MainActivity layout update

--- app/src/main/java/com/mlopez/interviewapp/MainActivity.kt
+++ app/src/main/java/com/mlopez/interviewapp/MainActivity.kt
@@ -2,10 +2,18 @@ package com.mlopez.interviewapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.RecyclerView
+import com.mlopez.interviewapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var binding: ActivityMainBinding
+    lateinit var rvUsers: RecyclerView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
     }
 }
