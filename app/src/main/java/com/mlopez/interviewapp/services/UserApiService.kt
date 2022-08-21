a3929b1b915b032b25c2381b7e7b7b95ec16cb2c
Maximo Lopez <maximo.lopez@spatialnetworks.com>
2022-08-21 09:40:35 -0400

    UserApiService added

---
+++ app/src/main/java/com/mlopez/interviewapp/services/UserApiService.kt
@@ -0,0 +1,13 @@
+package com.mlopez.interviewapp.services
+
+import com.mlopez.interviewapp.models.ResponseModel
+import com.mlopez.interviewapp.models.User
+import retrofit2.Response
+import retrofit2.http.GET
+
+interface UserApiService {
+    @GET("user/all")
+    suspend fun getAllUsers(): Response<List<User>> {
+        return RetrofitInstance.api.getAllUsers()
+    }
+}
