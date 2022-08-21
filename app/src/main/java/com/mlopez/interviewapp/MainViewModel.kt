5cc007e8bbd2b0c2bbf18f9457b821c0684b0203
Maximo Lopez <maximo.lopez@spatialnetworks.com>
2022-08-21 10:09:17 -0400

    Update on MainViewModel

--- app/src/main/java/com/mlopez/interviewapp/MainViewModel.kt
+++ app/src/main/java/com/mlopez/interviewapp/MainViewModel.kt
@@ -1,6 +1,28 @@
 package com.mlopez.interviewapp
 
+import android.util.Log
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.mlopez.interviewapp.models.ResponseModel
+import com.mlopez.interviewapp.models.User
+import com.mlopez.interviewapp.repositories.UserApiRepository
+import kotlinx.coroutines.launch
 
-class MainViewModel: ViewModel() {
+class MainViewModel(private val repository: UserApiRepository): ViewModel() {
+    private var _response = MutableLiveData(ResponseModel())
+    val response: LiveData<ResponseModel> = _response
+
+    fun saveUser(value: ResponseModel) {
+        _response.value = value
+    }
+
+    fun fetchUsers() {
+        viewModelScope.launch {
+            val response = repository.getAllUsers()
+            Log.d("MainViewModel_fetchUser", response.toString())
+            _response.value = response.body()
+        }
+    }
 }
