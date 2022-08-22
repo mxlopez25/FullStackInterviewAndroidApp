af36d9ad92dc2f7907b8a3b68b5c9dc500bfef08
Maximo Lopez <maximo.lopez@spatialnetworks.com>
2022-08-21 23:33:25 -0400

    Update of MainActivity

--- app/src/main/java/com/mlopez/interviewapp/MainActivity.kt
+++ app/src/main/java/com/mlopez/interviewapp/MainActivity.kt
@@ -2,8 +2,14 @@ package com.mlopez.interviewapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.NavigationUI
 import androidx.recyclerview.widget.RecyclerView
 import com.mlopez.interviewapp.databinding.ActivityMainBinding
+import com.mlopez.interviewapp.factory.MainViewModelFactory
+import com.mlopez.interviewapp.repositories.UserApiRepository
 
 class MainActivity : AppCompatActivity() {
 
@@ -15,6 +21,23 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-//        val userApiRepository = UserApiRepository()
+        val userApiRepository = UserApiRepository()
+        val viewModelFactory = MainViewModelFactory(userApiRepository)
+        mainViewModel = ViewModelProvider(this, viewModelFactory)[MainViewModel::class.java]
+        mainViewModel.fetchUsers()
+
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.fragmentContainerView)
+                as NavHostFragment
+        val navController = navHostFragment.navController
+
+        val appBarConfiguration = AppBarConfiguration(
+            setOf(
+                R.id.usersFragment,
+                R.id.productsFragment
+            )
+        )
+
+        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration)
+        NavigationUI.setupWithNavController(binding.bottomNavigationView, navController)
     }
 }
