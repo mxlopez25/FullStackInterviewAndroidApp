005771400a7d5861cd599b4bd35e94e2eb19f8d2
Maximo Lopez <maximo.lopez@spatialnetworks.com>
2022-08-21 23:34:10 -0400

    ApiServices and ApiRepository added

--- app/src/main/java/com/mlopez/interviewapp/repositories/UserApiRepository.kt
+++ app/src/main/java/com/mlopez/interviewapp/repositories/UserApiRepository.kt
@@ -3,11 +3,12 @@ package com.mlopez.interviewapp.repositories
 import com.mlopez.interviewapp.models.ResponseModel
 import com.mlopez.interviewapp.models.User
 import com.mlopez.interviewapp.services.RetrofitInstance
+import retrofit2.Call
 import retrofit2.Response
 import retrofit2.Retrofit
 
 class UserApiRepository {
-    suspend fun getAllUsers(): Response<ResponseModel> {
+    suspend fun getAllUsers(): Call<ResponseModel> {
         return RetrofitInstance.api.getAllUsers()
     }
 }
--- app/src/main/java/com/mlopez/interviewapp/services/UserApiService.kt
+++ app/src/main/java/com/mlopez/interviewapp/services/UserApiService.kt
@@ -2,12 +2,13 @@ package com.mlopez.interviewapp.services
 
 import com.mlopez.interviewapp.models.ResponseModel
 import com.mlopez.interviewapp.models.User
+import retrofit2.Call
 import retrofit2.Response
 import retrofit2.http.GET
 
 interface UserApiService {
-    @GET("user/all")
-    suspend fun getAllUsers(): Response<ResponseModel> {
+    @GET("all/")
+    suspend fun getAllUsers(): Call<ResponseModel> {
         return RetrofitInstance.api.getAllUsers()
     }
 }
