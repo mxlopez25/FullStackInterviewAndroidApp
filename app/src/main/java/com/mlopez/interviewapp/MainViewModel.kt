2bfd16d9e5a4c8b3b8d6fbc910e15353f37f4f48
Maximo Lopez <maximo.lopez@spatialnetworks.com>
2022-08-21 23:34:27 -0400

    ViewModel and Factory added

--- app/src/main/java/com/mlopez/interviewapp/MainViewModel.kt
+++ app/src/main/java/com/mlopez/interviewapp/MainViewModel.kt
@@ -9,6 +9,9 @@ import com.mlopez.interviewapp.models.ResponseModel
 import com.mlopez.interviewapp.models.User
 import com.mlopez.interviewapp.repositories.UserApiRepository
 import kotlinx.coroutines.launch
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
 class MainViewModel(private val repository: UserApiRepository): ViewModel() {
     private var _response = MutableLiveData(ResponseModel())
@@ -22,7 +25,17 @@ class MainViewModel(private val repository: UserApiRepository): ViewModel() {
         viewModelScope.launch {
             val response = repository.getAllUsers()
             Log.d("MainViewModel_fetchUser", response.toString())
-            _response.value = response.body()
+            response.enqueue(object : Callback<ResponseModel> {
+                override fun onFailure(call: Call<ResponseModel>, t: Throwable) {
+                    Log.e("MainViewModel", "Call to server Failed")
+                }
+
+                override fun onResponse(call: Call<ResponseModel>, response: Response<ResponseModel>) {
+                    Log.d("MainViewModel", response.body().toString())
+                }
+
+            })
+//            _response.value = response.body()
         }
     }
 }
--- app/src/main/java/com/mlopez/interviewapp/factory/MainViewModelFactory.kt
+++ app/src/main/java/com/mlopez/interviewapp/factory/MainViewModelFactory.kt
@@ -3,10 +3,11 @@ package com.mlopez.interviewapp.factory
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.viewmodel.CreationExtras
+import com.mlopez.interviewapp.MainViewModel
 import com.mlopez.interviewapp.repositories.UserApiRepository
 
 class MainViewModelFactory(private val repository: UserApiRepository): ViewModelProvider.Factory {
     override fun <T : ViewModel> create(modelClass: Class<T>, extras: CreationExtras): T {
-        return MainViewModelFactory(repository) as T
+        return MainViewModel(repository) as T
     }
 }
